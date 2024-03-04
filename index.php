b53a02110fa2d4fc67e70e1e0557c45901a3bbfb
sourav03242 <mondalsourav03242@gmail.com>
2024-03-04 22:03:49 +0530

    php tutorial

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP tutorial</title>
+</head>
+<body>
+    <div class="container">This is my first php website
+    <?php
+      echo "hello world and this is printed using php";
+    //   variable in php
+      $variable1=23;
+      $variable2=45;
+      echo $variable1;
+      echo $variable2;
+      echo $variable1+$variable2;
+      echo "<br>";
+      echo "the sum of variable one and two is ";
+      echo $variable1+$variable2;
+      echo "<br>";
+      echo "<h1>hii i am sourav</h1>";
+      echo var_dump(1==4);
+      echo var_dump(1!=4);
+      echo var_dump(1>4);
+      echo var_dump(1<4);
+
+    //   operator in php
+
+    ?>
+    </div>
+    
+</body>
+</html>
